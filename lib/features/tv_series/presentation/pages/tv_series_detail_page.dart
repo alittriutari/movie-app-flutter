e99a779d2d28dc254c7673c4f7add23a2e3f0d86
alittriutari <alittriutari@gmail.com>
2022-04-12 21:29:51 +0800

    add detail page provider

--- lib/features/tv_series/presentation/pages/tv_series_detail_page.dart
+++ lib/features/tv_series/presentation/pages/tv_series_detail_page.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/features/movies/domain/entities/genre.dart';
+import 'package:ditonton/features/tv_series/domain/entities/tv_series_detail.dart';
 import 'package:ditonton/features/tv_series/presentation/providers/tv_series_detail_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:provider/provider.dart';
 
 class TvSeriesDetailPage extends StatefulWidget {
@@ -17,11 +23,184 @@ class _TvSeriesDetailPageState extends State<TvSeriesDetailPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.microtask(() => Provider.of<TvSeriesDetailNotifier>(context, listen: false));
+    print(widget.id);
+    Future.microtask(() => Provider.of<TvSeriesDetailNotifier>(context, listen: false).fetchTvSeriesDetail(widget.id));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Consumer<TvSeriesDetailNotifier>(
+        builder: (context, provider, child) {
+          if (provider.tvSeriesState == RequestState.Loading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (provider.tvSeriesState == RequestState.Loaded) {
+            final tvSeries = provider.tvSeries;
+            return SafeArea(
+              child: DetailTvSeriesContent(
+                tvSeries,
+              ),
+            );
+          } else {
+            return Text(provider.message);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DetailTvSeriesContent extends StatelessWidget {
+  final TvSeriesDetail tvSeries;
+
+  DetailTvSeriesContent(
+    this.tvSeries,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        CachedNetworkImage(
+          imageUrl: 'https://image.tmdb.org/t/p/w500${tvSeries.posterPath}',
+          width: screenWidth,
+          placeholder: (context, url) => Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 48 + 8),
+          child: DraggableScrollableSheet(
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: kRichBlack,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                ),
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  top: 16,
+                  right: 16,
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tvSeries.title,
+                              style: kHeading5,
+                            ),
+                            ElevatedButton(
+                              onPressed: () async {},
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(Icons.add),
+                                  Text('Watchlist'),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              _showGenres(tvSeries.genres),
+                            ),
+                            Text(
+                              _showDuration(tvSeries.runtime),
+                            ),
+                            Row(
+                              children: [
+                                RatingBarIndicator(
+                                  rating: tvSeries.voteAverage / 2,
+                                  itemCount: 5,
+                                  itemBuilder: (context, index) => Icon(
+                                    Icons.star,
+                                    color: kMikadoYellow,
+                                  ),
+                                  itemSize: 24,
+                                ),
+                                Text('${tvSeries.voteAverage}')
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Overview',
+                              style: kHeading6,
+                            ),
+                            Text(
+                              tvSeries.overview,
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Recommendations',
+                              style: kHeading6,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        color: Colors.white,
+                        height: 4,
+                        width: 48,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            // initialChildSize: 0.5,
+            minChildSize: 0.25,
+            // maxChildSize: 1.0,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundColor: kRichBlack,
+            foregroundColor: Colors.white,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  String _showGenres(List<Genre> genres) {
+    String result = '';
+    for (var genre in genres) {
+      result += genre.name + ', ';
+    }
+
+    if (result.isEmpty) {
+      return result;
+    }
+
+    return result.substring(0, result.length - 2);
+  }
+
+  String _showDuration(int runtime) {
+    final int hours = runtime ~/ 60;
+    final int minutes = runtime % 60;
+
+    if (hours > 0) {
+      return '${hours}h ${minutes}m';
+    } else {
+      return '${minutes}m';
+    }
   }
 }
--- lib/features/tv_series/presentation/providers/tv_series_detail_notifier.dart
+++ lib/features/tv_series/presentation/providers/tv_series_detail_notifier.dart
@@ -1,3 +1,5 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/features/tv_series/domain/entities/tv_series_detail.dart';
 import 'package:ditonton/features/tv_series/domain/usecases/get_tv_series_detail.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -5,4 +7,28 @@ class TvSeriesDetailNotifier extends ChangeNotifier {
   final GetTvSeriesDetail getTvSeriesDetail;
 
   TvSeriesDetailNotifier({required this.getTvSeriesDetail});
+
+  late TvSeriesDetail _tvSeries;
+  TvSeriesDetail get tvSeries => _tvSeries;
+
+  RequestState _tvSeriesState = RequestState.Empty;
+  RequestState get tvSeriesState => _tvSeriesState;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchTvSeriesDetail(int id) async {
+    _tvSeriesState = RequestState.Loading;
+    notifyListeners();
+    final detailResult = await getTvSeriesDetail.execute(id);
+    detailResult.fold((failure) {
+      _tvSeriesState = RequestState.Error;
+      _message = failure.message;
+      notifyListeners();
+    }, (tvSeries) {
+      _tvSeriesState = RequestState.Loaded;
+      _tvSeries = tvSeries;
+      notifyListeners();
+    });
+  }
 }
