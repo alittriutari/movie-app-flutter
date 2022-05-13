adc5c72f0d23b32cf3fb1799d785cd47c8daffce
alittriutari <alittriutari@gmail.com>
2022-05-13 21:30:05 +0800

    Search usecase test

--- lib/features/search/domain/repositories/search_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:movie_app/common/failure.dart';
-import 'package:movie_app/features/movies/domain/entities/movie.dart';
-import 'package:movie_app/features/tv_series/domain/entities/tv_series.dart';
-
-abstract class SearchRepository {
-  Future<Either<Failure, List<Movie>>> searchMovies(String query);
-  Future<Either<Failure, List<TvSeries>>> searchTvSeries(String query);
-}
