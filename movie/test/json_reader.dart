fd94c74fdb375d3aa14f9931bc572142359a16f8
alittriutari <alit.appkey@gmail.com>
2022-07-14 14:21:21 +0800

    update json reader

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -5,5 +5,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  if (dir.endsWith('movie')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  return File('$dir/movie/test/$name').readAsStringSync();
 }
--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -5,5 +5,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  if (dir.endsWith('tv_series')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  return File('$dir/tv_series/test/$name').readAsStringSync();
 }
